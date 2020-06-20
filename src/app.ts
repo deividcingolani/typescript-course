09eb38278c74020af67e675655113b6f68c0e10b
David Cingolani <cingolanidavid@gmail.com>
2020-06-20 16:50:30 -0300

    Rendering project item using a class.

--- src/app.ts
+++ src/app.ts
@@ -177,7 +177,26 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract configure(): void;
   abstract renderContent(): void;
 }
+//ProjectItem class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
 
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+    this.configure();
+    this.renderContent();
+  }
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector(
+      "h3"
+    )!.textContent = this.project.people.toString();
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
 // ProjectList class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -208,9 +227,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     listEl.innerHTML = "";
 
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement("li")!;
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
     }
   }
 
@@ -301,6 +318,5 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 
 const prjInput = new ProjectInput();
 const activePrjList = new ProjectList("active");
-/*
+
 const finishedPrjList = new ProjectList("finished");
-*/
